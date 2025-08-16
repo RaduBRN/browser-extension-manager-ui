34378a481b36a48678467bdfd65bcba2749144aa
Radu Baroncea <baroncea.radu@gmail.com>
2025-08-16 09:19:03 +0300

    fix responsive size

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div data-theme={darkMode && "dark"}>
-      <div className="font-display bg-light-gradient bg-dark-gradient flex flex-col items-center max-w-[375px] md:max-w-[768px] lg:max-w-full min-h-screen px-5 md:px-6 lg:px-32 py-6 lg:py-10 gap-8 md:gap-10 lg:gap-12">
+      <div className="font-display bg-light-gradient bg-dark-gradient flex flex-col items-center md:max-w-[768px] lg:max-w-full min-h-screen px-5 md:px-6 lg:px-32 py-6 lg:py-10 gap-8 md:gap-10 lg:gap-12">
         <Header darkMode={darkMode} setDarkMode={setDarkMode} />
         <Content />
       </div>
